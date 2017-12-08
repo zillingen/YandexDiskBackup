1d50e44efe749f5ab758dbd777545ffb5406d09e
Zillingen <zill8830@gmail.com>
2017-12-08 12:04:55 +0000

    Добавлены ф-ции добавления папки и получения информации о файле/папке

--- YandexDiskBackup.php
+++ YandexDiskBackup.php
@@ -6,7 +6,7 @@ $date = date('Ymd_His').'UTC'; // Дата создания бэкапа
 /**
  * Проверка подключения к диску и вывод базовой информации 
  */
-function checkDisk() {
+function getDiskInfo() {
   global $config;
 
   $ch = curl_init();
@@ -17,16 +17,53 @@ function checkDisk() {
   );
 
   $options = array(
-      CURLOPT_URL => 'https://cloud-api.yandex.net/v1/disk/',
-      CURLOPT_RETURNTRANSFER => TRUE,
-      CURLOPT_VERBOSE => FALSE,
-      CURLOPT_HTTPHEADER => $headers,
+      CURLOPT_URL             => 'https://cloud-api.yandex.net/v1/disk/',
+      CURLOPT_RETURNTRANSFER  => TRUE,
+      CURLOPT_VERBOSE         => FALSE,
+      CURLOPT_HTTPHEADER      => $headers,
     );
-
   curl_setopt_array($ch, $options);
 
   $body = curl_exec($ch);
   $res = curl_getinfo($ch); 
+  curl_close($ch);
+
+  if ($res['http_code'] === 200) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
+
+/**
+ * Получение информации о файле или папке
+ *
+ * @param string $path Путь к файлу/папке в папке приложения
+ */
+function getDiskFileInfo($path) {
+	global $config;
+	$baseUrl = 'https://cloud-api.yandex.net/v1/disk/resources?';
+	$url = $baseUrl . http_build_query(array(
+		'path'	=> 	'app:/'.$path
+	));
+
+  $ch = curl_init();
+
+  $headers = array(
+    'Authorization: OAuth '.$config['token'],
+    'Accept: application/json',
+  );
+
+  $options = array(
+      CURLOPT_URL             => $url,
+      CURLOPT_RETURNTRANSFER  => TRUE,
+      CURLOPT_VERBOSE         => TRUE,
+      CURLOPT_HTTPHEADER      => $headers,
+    );
+  curl_setopt_array($ch, $options);
+
+  $body = curl_exec($ch);
+  $res = curl_getinfo($ch);
   curl_close($ch);
 
   if ($res['http_code'] === 200) {
@@ -42,7 +79,38 @@ function checkDisk() {
  * @param string $name Имя директории
  */
 function createDiskDir($name) {
+  global $config;
+	$baseUrl = 'https://cloud-api.yandex.net/v1/disk/resources/?';
+	$url = $baseUrl . http_build_query(array(
+		'path'	=>	'app:/'.$name,
+	));
 
+  $ch = curl_init();
+
+  $headers = array(
+    'Authorization: OAuth '.$config['token'],
+    'Accept: application/json',
+  );
+
+	$options = array(
+      CURLOPT_URL             => $url,
+      CURLOPT_RETURNTRANSFER  => TRUE,
+      CURLOPT_VERBOSE         => FALSE,
+      CURLOPT_HTTPHEADER      => $headers,
+			CURLOPT_CUSTOMREQUEST   => 'PUT',
+    );
+  curl_setopt_array($ch, $options);
+
+	$body = curl_exec($ch);
+  $res = curl_getinfo($ch);
+  curl_close($ch);
+
+  // 201 - Created; 409 - Conflict (папка уже существует)
+	if ($res['http_code'] === 201 || $res['http_code'] === 409) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
 }
 
 /**
